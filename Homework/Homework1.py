5036bfa060857a893c4d9d824061b027efdbac34
Татьяна <tatana@MacBook-Air-Tatana.local>
2022-06-12 12:40:58 +0300

    Homework 1, 2

---
+++ Homework/Homework1.py
@@ -0,0 +1,15 @@
+#ДЗ №1
+print((2+1)*4)
+
+name = "John Snow"
+age = 29
+print(name)
+print(age)
+
+print("I'm a student and I'll become a 'strong' programmer")
+print("I'm a student and I'll become a \"strong\" programmer")  #I'm a student and I'll become a "strong" programmer
+print("C:\\Users\\GoodStudent")  #C:\Users\GoodStudent
+
+x = "My name is Agent Smith"
+print(x[1]) #y
+print(x[3:16:3]) #nesgt
